a821a664c6953b9af6a78896eb87e611b6230117
Ayush Gupta <ayushghd@gmail.com>
2019-01-26 15:02:42 +0530

    Update video_to_text.py

--- video_to_text.py
+++ video_to_text.py
@@ -1,20 +1,29 @@
 import os
+import time
 import requests 
+import tkinter as tk
 from selenium import webdriver
 from sys import platform
+import urllib.request
 from xml.etree import ElementTree
-
+from selenium.webdriver.support.wait import WebDriverWait
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 #BASE_DIR = os.path.dirname("/home/aayushshivam7/")
 DRIVER_NAME = "chromedriver.exe" if platform == "win32" else "chromedriver"
 DRIVER_DIR = os.path.join(BASE_DIR, "plugins", DRIVER_NAME)
 converter = "https://www.360converter.com/conversion/video2TextConversion?type=v2t"
 duration = '00:00:30'
-#JS_SCRIPT = 'if(yt.config_.TTS_URL.length) window.location.href=yt.config_.TTS_URL+"&kind=asr&fmt=srv1&lang=en"'
-# JS_SCRIPT = 'if(yt.config_.TTS_URL != "") window.location.href=yt.config_.TTS_URL+"&kind=asr&fmt=srv1&lang=en"'
 
-def convert_to_text(filePath):
+def wait_for_page_load(driver, timeout=30):
+        old_page = driver.find_element_by_tag_name('html')
+        yield
+        WebDriverWait(driver, timeout).until(
+            staleness_of(old_page)
+        )
 
+def convert_to_text(filePath, keyword):
+
+    filePath = "/home/priyanshu/Desktop/GTube/static/images/videoplayback.mp4"
     driver = webdriver.Chrome(DRIVER_DIR)
     driver.get(converter)
     radio = driver.find_element_by_id('srcLocal')
@@ -31,14 +40,40 @@ def convert_to_text(filePath):
 
     agree = driver.find_element_by_id('agreementCheck') 
     agree.click()
-
+    print(driver.current_url)
     submit = driver.find_element_by_id('StartConvert')
     submit.click()
-
+    element = WebDriverWait(driver, 60).until(
+        lambda x: x.find_element_by_id("finished"))
     
-    sleep(10)
+    new_driver = driver.window_handles[0]
+    print(driver.current_url)
+
+    driver.switch_to_window(new_driver)
+    button = driver.find_element_by_id('downloadlink_txt')
+    while not button.is_displayed() :
+        a = 10
+    time.sleep(1)
+    total = driver.find_element_by_xpath('//*[@id="conversion"]/div[2]/div[4]/button[1]')
+    total.click() 
+    
+    root = tk.Tk()
+    result = root.clipboard_get()
+    print(result)
+    some_list = result.splitlines()
+    matching = [s for s in some_list if keyword in s]
+    print(matching)
+    mylist = []
+    for x in matching :
+    	mylist.append(some_list[some_list.index(x) + 2].split('-')[0])
+    final_list = []
+    for x in mylist:
+        final_list.append(float(x[1:]))
+    print(final_list)
+    time.sleep(1)
     driver.quit()
+    return final_list
     # return transcribe_urlip
 
 
-convert_to_text("/home/aayushshivam7/Desktop/Hack36/Yoogle/videoplayback.mp4")
+#\convert_to_text("/home/priyanshu/Desktop/GTube/videoplayback.mp4", "you")
